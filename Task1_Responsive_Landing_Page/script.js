4382552e4c7dcbfeef0d2b466a77f7379e3a4dbe
Bipulkumar7739 <bipulkumariimt7739@gmail.com>
2025-08-11 08:40:46 -0700

    Add files via upload

---
+++ Task1_Responsive_Landing_Page/script.js
@@ -0,0 +1,9 @@
+
+window.addEventListener('scroll', function() {
+    let header = document.querySelector('.topMenu');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
